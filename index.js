7e2a95f27b733a9f9a13ded9c17de7099a69e454
gbraisin <gabriel.burrill@raisin.com>
2020-11-04 10:34:23 +0100

    block duplicate form submissions

---
+++ index.js
@@ -0,0 +1,2 @@
+var apiKey = "Bearer aPE7aKPlwjxGA-pksWTPVpk2UnRHfSelK-sktB8G0RE";
+var apiUrlCheckSubmissions = "https://api.netlify.com/api/v1/sites/faf3128f-dc96-41df-b57d-973056c7077d/submissions";
